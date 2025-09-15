135341870e47227a9b9befcb9f2a5f82dc4ddd0f
Billy Kore <billyimmcul2010@gmail.com>
2025-09-15 17:00:37 +0700

    Add user table creation and deletion scripts: implement up and down SQL for users table with necessary fields and constraints

---
+++ internal/pkg/db/migrations/000002_create_users_table.up.sql
@@ -0,0 +1,18 @@
+CREATE TABLE users
+(
+    id            BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id       UUID UNIQUE              DEFAULT gen_random_uuid(),
+    username      VARCHAR(50) UNIQUE  NOT NULL,
+    first_name    VARCHAR(100)        NOT NULL,
+    last_name     VARCHAR(100)        NOT NULL,
+    email         VARCHAR(255) UNIQUE NOT NULL,
+    phone_number  VARCHAR(20) UNIQUE  NOT NULL,
+    password_hash VARCHAR(255)        NOT NULL,
+    date_of_birth DATE                NOT NULL,
+    cif_number    VARCHAR(20) UNIQUE  NOT NULL,
+    last_login    TIMESTAMP,
+    status        VARCHAR(20)              DEFAULT 'active',
+    created_at    TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at    TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    deleted_at    TIMESTAMP WITH TIME ZONE DEFAULT NULL
+);
